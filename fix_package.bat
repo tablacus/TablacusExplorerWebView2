96cf29ef589287653c76f5d6804552fa20730907
Gaku <rxf10242@gmail.com>
2021-06-11 21:36:00 +0900

    21.6.11

--- fix_package.bat
+++ fix_package.bat
@@ -1,6 +1,6 @@
-xcopy /e /i .\packages\Microsoft.Web.WebView2.1.0.818.41\build\native .\packages\Microsoft.Web.WebView2.1.0.818.41\build
+rem xcopy /e /i .\packages\Microsoft.Web.WebView2.1.0.864.35\build\native .\packages\Microsoft.Web.WebView2.1.0.864.35\build
 mkdir .\x64
 mkdir .\x64\Debug
 mkdir .\Debug
-copy .\packages\Microsoft.Web.WebView2.1.0.818.41\build\x64\WebView2Loader.dll .\x64\Debug
-copy .\packages\Microsoft.Web.WebView2.1.0.818.41\build\x86\WebView2Loader.dll .\Debug
+copy .\packages\Microsoft.Web.WebView2.1.0.864.35\build\x64\WebView2Loader.dll .\x64\Debug
+copy .\packages\Microsoft.Web.WebView2.1.0.864.35\build\x86\WebView2Loader.dll .\Debug
