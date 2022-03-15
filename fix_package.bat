311d8726ad9d4255289d9c55765966c04a0d4233
Gaku <rxf10242@gmail.com>
2022-03-15 22:00:08 +0900

    22.3.15

--- fix_package.bat
+++ fix_package.bat
@@ -1,6 +1,6 @@
-rem xcopy /e /i .\packages\Microsoft.Web.WebView2.1.0.1072.54\build\native .\packages\Microsoft.Web.WebView2.1.0.1072.54\build
+rem xcopy /e /i .\packages\Microsoft.Web.WebView2.1.0.1150.38\build\native .\packages\Microsoft.Web.WebView2.1.0.1150.38\build
 mkdir .\x64
 mkdir .\x64\Debug
 mkdir .\Debug
-copy .\packages\Microsoft.Web.WebView2.1.0.1072.54\build\native\x64\WebView2Loader.dll .\x64\Debug
-copy .\packages\Microsoft.Web.WebView2.1.0.1072.54\build\native\x86\WebView2Loader.dll .\Debug
+copy .\packages\Microsoft.Web.WebView2.1.0.1150.38\build\native\x64\WebView2Loader.dll .\x64\Debug
+copy .\packages\Microsoft.Web.WebView2.1.0.1150.38\build\native\x86\WebView2Loader.dll .\Debug
